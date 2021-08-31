c8271c59966e445e298cef4759b93a7836880cbd
Yaroslav Rychikin <yaroslavrychikin@yandex.ru>
2021-08-31 16:36:28 +0400

    Second and third tasks

---
+++ src/main/kotlin/WallService.kt
@@ -0,0 +1,42 @@
+import kotlin.random.Random
+
+class WallService {
+    private var posts = emptyArray<Post>()
+    private var memoryOfId = emptyArray<Int>()
+
+    fun add(post: Post): Post {
+        if (posts.isNotEmpty()) for(postFor in  posts){
+            if (postFor.id == post.id) return postFor
+        }
+        for(postFor in  posts){
+            if (postFor.id == post.id) return post
+        }
+        var random = Random.nextInt(1,100_000_000)
+            posts.forEach r@{
+                if (it.id == random) {
+                    random = Random.nextInt(1,100_000_000)
+                    return@r
+                }
+            }
+        memoryOfId += random
+        val newPost = post.copy(id = random)
+        posts += newPost
+        return newPost
+    }
+
+    fun update(post: Post): Boolean {
+        for (postFor in posts){
+            if (post.id == postFor.id){
+                val newPost = postFor.copy(createdBy = 9, text = "OOO", replyOwnerId =  10,
+                    replyPostId = 50, friendsOnly = true, comments = 100, likes = 200, reposts = 6, views = 2500,
+                    signerId = 0, markedAsAds = true, iaFavorite = false, donut = false, postponed_id = 200)
+                posts[posts.indexOf(postFor)] = newPost
+                return true
+            }
+        }
+        return false
+    }
+
+    fun getIdFromMemory(index: Int): Int = if (index >= memoryOfId.size || index < 0) 0 else memoryOfId[index]
+
+}
---
+++ src/test/kotlin/WallServicesTest.kt
@@ -0,0 +1,142 @@
+import org.junit.Assert.*
+import org.junit.Test
+
+class WallServicesTest {
+
+    @Test
+    fun add() {
+        val service = WallService()
+        val result = service.add(Post(ownerId = 78 ,
+            fromId = 78,
+            createdBy = 8,
+            date = 5,
+            text = "Привет",
+            replyOwnerId =  10,
+            replyPostId = 8,
+            friendsOnly = true,
+            comments =899,
+            copyright = "Yes",
+            likes = 6,
+            reposts = 0,
+            views = 100,
+            postType = "post",
+            signerId = 0,
+            canPin = true,
+            canDelete = true,
+            canEdit = true,
+            isPinned = true,
+            markedAsAds = false,
+            iaFavorite = true,
+            donut = false,
+            postponed_id = 99)).id
+        assert(result in 1..100_000_000)
+    }
+
+    @Test
+    fun updateExisting() {
+        val service = WallService()
+        service.add(Post(ownerId = 78 ,
+            fromId = 78,
+            createdBy = 8,
+            date = 16,
+            text = "Привет",
+            replyOwnerId =  10,
+            replyPostId = 8,
+            friendsOnly = false,
+            comments = 99,
+            copyright = "Yes",
+            likes = 98,
+            reposts = 3,
+            views = 2000,
+            postType = "post",
+            signerId = 0,
+            canPin = true,
+            canDelete = true,
+            canEdit = true,
+            isPinned = true,
+            markedAsAds = false,
+            iaFavorite = true,
+            donut = true,
+            postponed_id = 99))
+        val update = Post(service.getIdFromMemory(0),
+            ownerId = 78 ,
+            fromId = 78,
+            createdBy = 8,
+            date = 16,
+            text = "Привет",
+            replyOwnerId =  10,
+            replyPostId = 8,
+            friendsOnly = false,
+            comments = 99,
+            copyright = "Yes",
+            likes = 98,
+            reposts = 3,
+            views = 2000,
+            postType = "post",
+            signerId = 0,
+            canPin = true,
+            canDelete = true,
+            canEdit = true,
+            isPinned = true,
+            markedAsAds = false,
+            iaFavorite = true,
+            donut = true,
+            postponed_id = 99)
+        val result = service.update(update)
+        assertTrue(result)
+    }
+
+    @Test
+    fun updateFail() {
+        val service = WallService()
+        service.add(Post(ownerId = 78 ,
+            fromId = 78,
+            createdBy = 8,
+            date = 16,
+            text = "Привет",
+            replyOwnerId =  10,
+            replyPostId = 8,
+            friendsOnly = false,
+            comments = 99,
+            copyright = "Yes",
+            likes = 98,
+            reposts = 3,
+            views = 2000,
+            postType = "post",
+            signerId = 0,
+            canPin = true,
+            canDelete = true,
+            canEdit = true,
+            isPinned = true,
+            markedAsAds = false,
+            iaFavorite = true,
+            donut = true,
+            postponed_id = 99))
+        val update = Post(id = 77,
+            ownerId = 78 ,
+            fromId = 78,
+            createdBy = 8,
+            date = 16,
+            text = "Привет",
+            replyOwnerId =  10,
+            replyPostId = 8,
+            friendsOnly = false,
+            comments = 99,
+            copyright = "Yes",
+            likes = 98,
+            reposts = 3,
+            views = 2000,
+            postType = "post",
+            signerId = 0,
+            canPin = true,
+            canDelete = true,
+            canEdit = true,
+            isPinned = true,
+            markedAsAds = false,
+            iaFavorite = true,
+            donut = true,
+            postponed_id = 99)
+        val result = service.update(update)
+        assertFalse(result)
+    }
+}
